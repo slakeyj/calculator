c5b57f9443318f78e4786004431a1aebecbb8d44
Susanna Lakey <susannalakey@gmail.com>
2020-11-02 11:34:36 -0800

    basic styling

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import './Button.css'
 
-
-// const gridCellStyle = {
-//   flexGrow: '1',
-//   height: '100px',
-//   width: '50px',
-//   margin: '5px'
-// }
-
 const Button = ({ onClick, children }) => {
   return <button className="button" onClick={onClick}>{children}</button>
 }
