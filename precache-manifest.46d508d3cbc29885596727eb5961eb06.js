54ee5395a64951bc8d840c5ad397093d559f65b4
Susanna Lakey <susannalakey@gmail.com>
2020-11-05 08:10:47 -0800

    Updates

---
+++ precache-manifest.46d508d3cbc29885596727eb5961eb06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b41ab4b9da6869cf12241d34e138aa5",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "0e911b68c07c70c51d42",
+    "url": "/calculator/static/css/main.fc7c4b47.chunk.css"
+  },
+  {
+    "revision": "1fd9b0ae6dda1819521a",
+    "url": "/calculator/static/js/2.f2ff28fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calculator/static/js/2.f2ff28fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e911b68c07c70c51d42",
+    "url": "/calculator/static/js/main.bfe79b8d.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
